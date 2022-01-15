98242f6c7854ea58b11eca02aab05954788f637a
RajuMudhiraj <rajuthecodingcreature@gmail.com>
2022-01-15 21:07:49 +0530

    Learned react routeer

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import {Outlet, Link} from 'react-router-dom';
+import './App.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Bookkeeper!</h1>
+      <nav 
+      style={{
+        borderBottom: "solid 1px",
+        paddingBottom: "1rem"
+      }}>
+
+        <Link to="/invoices">Invoices</Link>
+        <br />
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+      <Outlet />
+    </div>
+  );
+}
